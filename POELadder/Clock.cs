bc1569a672499f16e293fea7c94b7932ac99613b
uweenukr <uweenukr@gmail.com>
2013-03-21 15:46:41 -0700

    First Commit

---
+++ POELadder/Clock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace PoELadder
+{
+    class Clock
+    {
+
+        //Convert Time from JSON document and returns a DateTime. Can be null. Returned as MinValue
+        public static DateTime FormatPOEDate(String POEDate)
+        {
+            //Null happens for perm Leagues
+            if (string.IsNullOrEmpty(POEDate))
+            {
+                return DateTime.MinValue;
+            }
+
+            else
+            {
+                //Year:
+                int POEYear = Int32.Parse(POEDate.Substring(0, 4));
+
+                //Month:
+                int POEMonth = Int32.Parse(POEDate.Substring(5, 2));
+
+                //Day:
+                int POEDay = Int32.Parse(POEDate.Substring(8, 2));
+
+                //Hour:
+                int POEHour = Int32.Parse(POEDate.Substring(11, 2));
+
+                //Minute:
+                int POEMinute = Int32.Parse(POEDate.Substring(14, 2));
+
+                //Second:
+                int POESecond = Int32.Parse(POEDate.Substring(17, 2));
+
+                //Combine to create the DateTime Object
+                DateTime result = new DateTime(POEYear, POEMonth, POEDay, POEHour, POEMinute, POESecond);
+
+                return result;
+
+            }
+        }
+
+    }
+}
---
+++ POELadder/JSON/PathOfExileJSONLadderAll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PoELadder.JSON
+{
+    public class PathOfExileJSONLadderAll
+    {
+        public string id { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public bool @event { get; set; }
+        public string registerAt { get; set; }
+        public string startAt { get; set; }
+        public string endAt { get; set; }
+        public List<object> rules { get; set; }
+        public List<object> ladder { get; set; }
+    }
+}
